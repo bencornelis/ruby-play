af8af51276a57b418fb65979095ddc838477115b
Ben Cornelis <bencornelis02@gmail.com>
2017-08-06 12:34:05 -0700

    add 1-d pascals triangle

--- lib/pascal.rb
+++ lib/pascal.rb
@@ -1,3 +1,4 @@
+# 2-d array
 def pascals_triangle(n)
   (n - 1).times.inject([[1]]) { |triangle|
     triangle << next_row(triangle) }
@@ -5,4 +6,11 @@ end
 
 def next_row(triangle)
   [0, *triangle.last, 0].each_cons(2).map { |x, y| x + y }
+end
+
+# 1-d array
+def pascal(n)
+  sum_two = -> x, y { x + y }
+  (1...n).inject([1]) { |tr, k|
+    tr + [0, *tr.last(k), 0].each_cons(2).map(&sum_two) }
 end
